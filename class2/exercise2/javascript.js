e7651bf151ab7756c2cb4fbba4ecbc63e58fbf19
Corey Nilan <holodeck@Coreys-MacBook-Pro.local>
2015-02-16 17:16:31 -0500

    change function

--- class2/exercise2/javascript.js
+++ class2/exercise2/javascript.js
@@ -12,32 +12,37 @@ function calculate(){
     } 
 }
 
-function favoriteThings(){
-    var favoriteThings = ['Rabbits', 'Orange', 'Yogurt', 'Brussel Sprouts', 'Otters'];
-    var result = 'My favorite things are: ';
-    for (var i = 0; i<favoriteThings.length; i++){
-        if (i < favoriteThings.length - 1){
-            result += favoriteThings[i] + ', ';
-        }else{
-            result += "and " + favoriteThings[i] + '.';
+function studentAverages() {
+    var allAverages = [95, 90, 93, 52, 70, 88];
+    var goodAverages = [];
+    var badAverages = [];
+    for (var i = 0; i < allAverages.length; i++) {
+        if (allAverages[i] >= 80) {
+            goodAverages.push(allAverages[i]);
+        }
+        else {
+            badAverages.push(allAverages[i]);
         }
     }
-    alert(result);
+    var sentence = goodAverages.length + " students have averages above 80 and " + badAverages.length + " students have averages below 80.";
+    
+    alert(sentence);
 }
+
 function myFriends(){
     var friends = [
         {name: 'Santa Claus',
-        hair: 'red'},
+        hair: 'white'},
         {name: 'Easter Bunny',
         hair: 'brown'},
         {name: 'Tooth Fairy',
         hair: 'blue'}
     ];
-    var results = "My friends: "
+    var results = "My friends: ";
     for(var i = 0; i < friends.length; i++){
         results += describeFriend(friends[i]);
     }
-    alert(results)
+    alert(results);
 }
 function describeFriend(friend){
     return "My friend " + friend.name + " has " + friend.hair + " hair. ";
